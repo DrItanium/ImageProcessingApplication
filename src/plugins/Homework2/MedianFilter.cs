3b76b4490ad8fe046b7cc3114bf95717bbb4b8e4
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-20 17:08:43 -0800

    Optimized MedianFilter.cs
    I also removed a bizzare lock statement, I have no clue why I did it :/

--- src/plugins/Homework2/MedianFilter.cs
+++ src/plugins/Homework2/MedianFilter.cs
@@ -14,39 +14,35 @@ using System.Collections;
 
 namespace CS555.Homework2
 {
-  [Filter("Median Filter")]
-    public class MedianFilter : SpatialFilter
-  {
-    private List<byte> elements;
-    private object elementsLock;
-    public MedianFilter(string name) : base(name) 
-    {
-      elements = new List<byte>();
-      elementsLock = new object();
-    }
-    protected override byte Operation(int a, int b, int x, int y, byte[][] input, Hashtable values)
-    {
-      lock(elementsLock)
-      {
-        int width = input.Length;
-        int height = input[0].Length;
-        elements.Clear();
-        for(int s = -a; s < a; s++)
-        {
-          int wX = x + s;
-          if(wX < 0 || wX >= width)
-            continue;
-          for(int t = -b; t < b; t++)
-          {
-            int wY = y + t;
-            if(wY < 0 || wY >= height)
-              continue;
-            elements.Add(input[wX][wY]);
-          }
-        }
-        elements.Sort();
-        return elements[elements.Count >> 1];
-      }
-    }
-  }
+	[Filter("Median Filter")]
+		public class MedianFilter : SpatialFilter
+	{
+		private List<byte> elements;
+		public MedianFilter(string name) : base(name) 
+		{
+			elements = new List<byte>();
+		}
+		protected override byte Operation(int a, int b, int x, int y, byte[][] input, Hashtable values)
+		{
+			int width = input.Length;
+			int height = input[0].Length;
+			elements.Clear();
+			for(int s = -a; s < a; s++)
+			{
+				int wX = x + s;
+				if(wX < 0 || wX >= width)
+					continue;
+				byte[] iX = input[wX];
+				for(int t = -b; t < b; t++)
+				{
+					int wY = y + t;
+					if(wY < 0 || wY >= height)
+						continue;
+					elements.Add(iX[wY]);
+				}
+				elements.Sort();
+				return elements[elements.Count >> 1];
+			}
+		}
+	}
 }
