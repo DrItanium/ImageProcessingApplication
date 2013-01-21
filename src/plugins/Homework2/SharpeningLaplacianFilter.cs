2d9692a75669936bd29b02037aefb110996ac2a8
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-01-20 17:15:26 -0800

    Optimized SharpeningLaplacianFilter.cs

--- src/plugins/Homework2/SharpeningLaplacianFilter.cs
+++ src/plugins/Homework2/SharpeningLaplacianFilter.cs
@@ -15,52 +15,55 @@ using System.Collections;
 
 namespace CS555.Homework2
 {
-  [Filter("Sharpening Laplacian Filter")]
-    public class SharpeningLaplacianFilter : Filter
-  {
-    public override string InputForm { get { return null; } }
-    public SharpeningLaplacianFilter(string name) : base(name) { }
+	[Filter("Sharpening Laplacian Filter")]
+		public class SharpeningLaplacianFilter : Filter
+	{
+		public override string InputForm { get { return null; } }
+		public SharpeningLaplacianFilter(string name) : base(name) { }
 
-    public override Hashtable TranslateData(Hashtable input) { return input; }
-    public override byte[][] Transform(Hashtable input)
-    {
-      byte[][] image = (byte[][])input["image"];
-      int iWidth = image.Length;
-      int iHeight = image[0].Length;
-      byte[][] clone = new byte[iWidth][];      
-      for(int x = 0; x < iWidth; x++)
-      {
-        clone[x] = new byte[iHeight];
-        for(int y = 0; y < iHeight; y++)
-        {
-          int result = image[x][y];
-          result += -1 * Laplacian(image, x, y, iWidth, iHeight);
-          clone[x][y] = result < 0 ? (byte)0 : (byte)result;
-        }
-      }
-      return clone;
-    }
-		 	private static int Laplacian(byte[][] b, int x, int y, int width, int height)
+		public override Hashtable TranslateData(Hashtable input) { return input; }
+		public override byte[][] Transform(Hashtable input)
+		{
+			byte[][] image = (byte[][])input["image"];
+			int iWidth = image.Length;
+			int iHeight = image[0].Length;
+			byte[][] clone = new byte[iWidth][];      
+			for(int x = 0; x < iWidth; x++)
 			{
-				//fix this up...we don't need to do this every time
-				int xM1 = x - 1;
-				int xP1 = x + 1;
-				int yM1 = y - 1;
-				int yP1 = y + 1;
-				int f0 = 0; 
-				int f1 = 0;
-				int f2 = 0;
-				int f3 = 0;
-				int f4 = (int)(b[x][y]) << 2; //times 4
-				if(xP1 < width)
-					f0 = b[xP1][y];
-				if(xM1 >= 0)
-					f1 = b[xM1][y];
-				if(yP1 < height)
-					f2 = b[x][yP1];
-				if(yM1 >= 0)
-					f3 = b[x][yM1];
-				return f0 + f1 + f2 + f3 - f4;
+				byte[] q = new byte[iHeight];
+				byte[] iX = image[x];
+				for(int y = 0; y < iHeight; y++)
+				{
+					int result = iX[y];
+					result += -1 * Laplacian(image, x, y, iWidth, iHeight);
+					q[y] = result < 0 ? (byte)0 : (byte)result;
+				}
+				clone[x] = q;
 			}
-  }
+			return clone;
+		}
+		private static int Laplacian(byte[][] b, int x, int y, int width, int height)
+		{
+			//fix this up...we don't need to do this every time
+			byte[] bX = b[x];
+			int xM1 = x - 1;
+			int xP1 = x + 1;
+			int yM1 = y - 1;
+			int yP1 = y + 1;
+			int f0 = 0; 
+			int f1 = 0;
+			int f2 = 0;
+			int f3 = 0;
+			int f4 = (int)(bX[y]) << 2; //times 4
+			if(xP1 < width)
+				f0 = b[xP1][y];
+			if(xM1 >= 0)
+				f1 = b[xM1][y];
+			if(yP1 < height)
+				f2 = bX[yP1];
+			if(yM1 >= 0)
+				f3 = bX[yM1];
+			return f0 + f1 + f2 + f3 - f4;
+		}
+	}
 }
