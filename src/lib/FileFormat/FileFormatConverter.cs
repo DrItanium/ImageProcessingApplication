d0e8d8fb711e8b50edde1617afbf11a0cace89ac
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-16 23:17:33 -0700

    Updated FileFormatConverter.cs to support saving and querying
    This makes it possible to query to find out information about the file format
    converter. Messages for supports-loading, supports-saving, get-form-code, and
    get-filter-string as well as load and save.

--- src/lib/FileFormat/FileFormatConverter.cs
+++ src/lib/FileFormat/FileFormatConverter.cs
@@ -15,10 +15,36 @@ namespace Libraries.FileFormat
 {
   public abstract class FileFormatConverter : Plugin 
   {
+    public abstract bool SupportsSaving { get; }
+    public abstract bool SupportsLoading { get; }
     public abstract string FilterString { get; }
     public abstract string FormCode { get; }
     protected FileFormatConverter(string name) : base(name) { }
-    public abstract Color[][] Process(Hashtable input);
+    public virtual object Process(Hashtable input) 
+    {
+      string action = (string)input["action"];
+      switch(action.ToLower()) 
+      {
+        case "load":
+          return Load(input);
+        case "save":
+          Save(input);
+          return null;
+        case "supports-loading":
+          return SupportsLoading;
+        case "supports-saving":
+          return SupportsSaving;
+        case "get-filter-string":
+          return FilterString;
+        case "get-form-code":
+          return FormCode;
+        default:
+          throw new ArgumentException(
+              string.Format("ERROR: Provided action {0} isn't valid", action));
+      }
+    }
+    public abstract Color[][] Load(Hashtable input);
+    public abstract void Save(Hashtable input);
     public override Message Invoke(Message input) 
     {
       return new Message(Guid.NewGuid(), 
