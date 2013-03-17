3b855b86d71dcc8ce788751d934a99a5eedaaf70
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-17 15:05:50 -0700

    Added a ByteVisualizer

---
+++ src/formats/binary/ByteVisualizer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using Libraries.FileFormat;
+using System.Drawing;
+
+
+namespace Formats.Binary
+{
+  [FileFormat("Visualize binary file [bytes] (*.*)")]
+  public class ByteVisualizer : BinaryVisualConverter
+  {
+    protected override int DivisorFactor { get { return 1; } }
+    public ByteVisualizer(string name) : base(name) { }
+    protected override Color GetPixel(FileStream fs)
+    {
+      int result = fs.ReadByte();
+      if(result == -1)
+      {
+        return Color.Black;
+      }
+      else
+      {
+        return Color.FromArgb(255, result, result, result);
+      }
+    }
+
+  }
+}
