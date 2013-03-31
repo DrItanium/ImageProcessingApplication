5d251d4538d729b00605d5c81521ae4966d963f9
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-31 15:07:54 -0700

    Added SquareDividerConverter class
    This class is meant to handle the cases where the tiles are squares. This makes
    it so that only one property has to be overriden. This property is the side
    length which represents the length (in pixels) of a side of the square.

---
+++ src/formats/divide/SquareDividerConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using Libraries.FileFormat;
+using System.Drawing;
+
+namespace Formats.Divide 
+{
+	///<summary>
+	///Base class for all Image division format converters
+	///</summary>
+	public abstract class SquareDividerConverter : ImageDividerConverter 
+	{
+		public abstract int SideLength { get; }
+		public sealed override int DivideWidth { get { return SideLength; } }
+		public sealed override int DivideHeight { get { return SideLength; } }
+		protected SquareDividerConverter(string name) : base(name) { }
+	}
+
+}
