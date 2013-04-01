568d4f813b6d35ce624e6598367cf4b3b13fc8b7
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-31 22:28:49 -0700

    Modified GlobalHistogramEqualization to use the ColorHistogram class

--- src/filters/Homework1/GlobalHistogramEqualization.cs
+++ src/filters/Homework1/GlobalHistogramEqualization.cs
@@ -14,38 +14,41 @@ using System.Collections;
 
 namespace CS555.Homework1
 {
-  [Filter("Global Histogram Equalization")]
-    public class GlobalHistogramEqualization : ImageFilter
-  {
-    public override string InputForm { get { return null; } }
-    public GlobalHistogramEqualization(string name) : base(name) { }
+	[Filter("Global Histogram Equalization")]
+		public class GlobalHistogramEqualization : ImageFilter
+	{
+		public override string InputForm { get { return null; } }
+		public GlobalHistogramEqualization(string name) : base(name) { }
 
-    public override Hashtable TranslateData(Hashtable source)
-    {
-      return source;
-    }
-    public override int[][] TransformImage(Hashtable source)
-    {
-      if(source == null)
-        return null; 
-      else
-      {
-        int[][] input = (int[][])source["image"];
+		public override Hashtable TranslateData(Hashtable source)
+		{
+			return source;
+		}
+		public override int[][] TransformImage(Hashtable source)
+		{
+			if(source == null)
+				return null; 
+			else
+			{
+				int[][] input = (int[][])source["image"];
 				//the histogram object automatically creates a pixel intensity
 				//distribution upon creation. It also computes the global equalized
 				//intensity based upon it's frequency in the given image. 
-        Histogram h = new Histogram(input);
-        for(int x = 0; x < input.Length; x++)
-        {
+				ColorHistogram ch = new ColorHistogram(input);
+				for(int x = 0; x < input.Length; x++)
+				{
 					int[] line = input[x];
-          for(int y = 0; y < input[y].Length; y++)
-          {
+					for(int y = 0; y < input[y].Length; y++)
+					{
 						//we just replace the previous pixels with the new ones 
-						line[y] = histogram.GlobalEqualizedIntensity[line[y]];
-          }
-        }
-        return input;
-      }
-    }
-  }
+						Color c = Color.FromArgb(line[y]);
+						line[y] = Color.FromArgb(255,ch.Red.GlobalEqualizedIntensity[c.R],
+								ch.Green.GlobalEqualizedIntensity[c.G],
+								ch.Blue.GlobalEqualizedIntensity[c.B]).ToArgb();
+					}
+				}
+				return input;
+			}
+		}
+	}
 }
