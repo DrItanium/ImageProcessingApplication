655509c30b9bd10add1287b270f82c4bc850e1f3
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-01 00:12:44 -0700

    Modified the tuple to be passed across appdomain bounds for file formats
    The can load and can save fields are passed across now

--- src/lib/FileFormat/FileFormatBackingStore.cs
+++ src/lib/FileFormat/FileFormatBackingStore.cs
@@ -12,27 +12,28 @@ using Frameworks.Plugin;
 
 namespace Libraries.FileFormat 
 {
-  public static class FileFormatLoaderBackingStore
-  {
-    private static Dictionary<Guid, FileFormatLoader> pluginEnvironments;
-    static FileFormatLoaderBackingStore()
-    {
-      pluginEnvironments = new Dictionary<Guid, FileFormatLoader>();
-    }
-    public static Message Invoke(Guid targetPluginGroup, Message input)
-    {
-      return pluginEnvironments[targetPluginGroup].Invoke(input);
-    }
-    public static Tuple<string, string, string, Guid> GetPlugin(Guid targetGuid, Guid pluginGuid)
-    {
-      FileFormatConverter target = (FileFormatConverter)pluginEnvironments[targetGuid][pluginGuid];
-      return new Tuple<string, string, string, Guid>(target.Name, target.FilterString, target.FormCode, target.ObjectID);
-    }
-    public static Tuple<Guid, Guid[]> LoadPlugins(string path)
-    {
-      FileFormatLoader pl = new FileFormatLoader(path);
-      pluginEnvironments.Add(pl.ObjectID, pl); 
-      return new Tuple<Guid,Guid[]>(pl.ObjectID, pl.Names.ToArray());
-    }
-  }
+	public static class FileFormatLoaderBackingStore
+	{
+		private static Dictionary<Guid, FileFormatLoader> pluginEnvironments;
+		static FileFormatLoaderBackingStore()
+		{
+			pluginEnvironments = new Dictionary<Guid, FileFormatLoader>();
+		}
+		public static Message Invoke(Guid targetPluginGroup, Message input)
+		{
+			return pluginEnvironments[targetPluginGroup].Invoke(input);
+		}
+		public static Tuple<string, string, string, Guid, Tuple<bool, bool>> GetPlugin(Guid targetGuid, Guid pluginGuid)
+		{
+			FileFormatConverter target = (FileFormatConverter)pluginEnvironments[targetGuid][pluginGuid];
+			return new Tuple<string, string, string, Guid, Tuple<bool,bool>>(target.Name, target.FilterString, 
+					target.FormCode, target.ObjectID, new Tuple<bool, bool>(target.CanLoad, target.CanSave));
+		}
+		public static Tuple<Guid, Guid[]> LoadPlugins(string path)
+		{
+			FileFormatLoader pl = new FileFormatLoader(path);
+			pluginEnvironments.Add(pl.ObjectID, pl); 
+			return new Tuple<Guid,Guid[]>(pl.ObjectID, pl.Names.ToArray());
+		}
+	}
 }
