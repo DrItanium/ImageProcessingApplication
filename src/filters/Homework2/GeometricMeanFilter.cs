972cb6e681336fdd313953366e15d22b5f4e73a0
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-02 18:14:02 -0700

    GeometricMeanFilter now supports color images

--- src/filters/Homework2/GeometricMeanFilter.cs
+++ src/filters/Homework2/GeometricMeanFilter.cs
@@ -15,34 +15,49 @@ using System.Collections;
 
 namespace CS555.Homework2
 {
-	[Filter("Geometric Mean Filter")]
-		public class GeometricMeanFilter : SpatialFilter
-	{
-		public GeometricMeanFilter(string name) : base(name) { }
-		protected override byte Operation(int a, int b, int x, int y, byte[][] input, Hashtable _)
-		{
+  [Filter("Geometric Mean Filter")]
+    public class GeometricMeanFilter : SpatialFilter
+  {
+    public GeometricMeanFilter(string name) : base(name) { }
+    protected override int Operation(int a, int b, int x, int y, int[][] input, Hashtable _)
+    {
       int width = input.Length;
       int height = input[0].Length;
-			int total = 1;
-			int size = 0;
-			for(int s = -a; s < a; s++)
-			{
-				int wX = x + s;
-				if(wX < 0 || wX >= width)
-					continue;
-				byte[] iX = input[wX];
-				for(int t = -b; t < b; t++)
-				{
-					int wY = y + t;
-					if(wY < 0 || wY >= height)
-						continue;
-					total *= iX[wY];
-					size++;
-				}
-			}
-			double power = (1.0 / (double)(size));		
-			double result = Math.Pow(total, power);
-			return (byte)result;
-		}
-	}
+      int totalRed = 1,
+          totalGreen = 1,
+          totalBlue = 1;
+      int size = 0;
+      for(int s = -a; s < a; s++)
+      {
+        int wX = x + s;
+        if(wX < 0 || wX >= width)
+          continue;
+        byte[] iX = input[wX];
+        for(int t = -b; t < b; t++)
+        {
+          int wY = y + t;
+          if(wY < 0 || wY >= height)
+            continue;
+          Color c = Color.FromArgb(iX[wY]);
+          totalRed *= c.R;
+          totalBlue *= c.B;
+          totalGreen *= c.G;
+          size++;
+        }
+      }
+      double power = (1.0 / (double)(size));		
+      if(totalRed == totalBlue && totalBlue == totalGreen) 
+      {
+        int result = (int)Math.Pow(totalRed, power);
+        return Color.FromArgb(255, result, result, result).ToArgb();
+      }
+      else
+      {
+        return Color.FromArgb(255, 
+            (int)Math.Pow(totalRed, power),
+            (int)Math.Pow(totalGreen, power),
+            (int)Math.Pow(totalBlue, power)).ToArgb();
+      }
+    }
+  }
 }
