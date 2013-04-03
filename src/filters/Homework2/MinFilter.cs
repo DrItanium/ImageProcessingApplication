426f0e9551a5e4ffc98004a247a21af18beada1a
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-02 19:00:09 -0700

    MinFilter now supports color images

--- src/filters/Homework2/MinFilter.cs
+++ src/filters/Homework2/MinFilter.cs
@@ -19,26 +19,31 @@ namespace CS555.Homework2
     public class MinFilter: SpatialFilter
   {
     public MinFilter(string name) : base(name) { }
-    protected override byte Operation(int a, int b, int x, int y, byte[][] input, Hashtable values)
+    protected override int Operation(int a, int b, int x, int y, int[][] input, Hashtable values)
     {
-      int width = input.Length;
-      int height = input[0].Length;
-      byte max = (byte)255;
+      int width = input.Length,
+          height = input[0].Length;
+      byte maxRed = (byte)255,
+           maxGreen = (byte)255,
+           maxBlue = (byte)255;
       for(int s = -a; s < a; s++)
       {
         int wX = x + s;
         if(wX < 0 || wX >= width)
           continue;
-				byte[] iX = input[wX];
+				int[] iX = input[wX];
         for(int t = -b; t < b; t++)
         {
           int wY = y + t;
           if(wY < 0 || wY >= height)
             continue;
-          max = Math.Min(max, iX[wY]);
+          Color c = Color.FromArgb(iX[wY]);
+          maxRed = Math.Min(maxRed, c.R);
+          maxGreen = Math.Min(maxGreen, c.G);
+          maxBlue = Math.Min(maxBlue, c.B);
         }
       }
-      return max;
+      return Color.FromArgb(maxRed, maxGreen, maxBlue).ToArgb();
     }
   }
 }
