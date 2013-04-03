abb805c947c120a9cf6be07b57e0335c8bfc1d51
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-02 17:12:01 -0700

    Simplified the code a bit in LocalHistogramEqualization

--- src/filters/Homework1/LocalHistogramEqualization.cs
+++ src/filters/Homework1/LocalHistogramEqualization.cs
@@ -81,14 +81,16 @@ namespace CS555.Homework1
 				int width = input.Length;
 				int height = input[0].Length;
         Func<Histogram, int, byte> fn = (hi,current) => (byte)(count * (double)hi[0, current + 1]);
+        Func<int,int,IEnumerable<int>> grabNeighborhood = (px, py) => input.GrabNeighborhood(px, py, width, height, mask);
 				for(int x = 0; x < width; x++)
 				{
 					int[] iX = input[x];
+          Func<int,IEnumerable<int>> gX = (py) => grabNeighborhood(x,py);
 					//close over these values 
 					for(int y = 0; y < height; y++)
 					{
             Color c = Color.FromArgb(iX[y]);
-						h.Repurpose(input.GrabNeighborhood(x, y, width, height, mask));
+            h.Repurpose(gX(y));
             iX[y] = Color.FromArgb(255, fn(h.Red, (int)c.R),
                                         fn(h.Green, (int)c.G),
                                         fn(h.Blue, (int)c.B)).ToArgb();
