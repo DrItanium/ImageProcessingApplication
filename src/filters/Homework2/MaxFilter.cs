040650ee09dacafe8f8d17a640c104b8630a46a2
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-02 18:44:49 -0700

    Max filter now supports color images

--- src/filters/Homework2/MaxFilter.cs
+++ src/filters/Homework2/MaxFilter.cs
@@ -19,26 +19,41 @@ namespace CS555.Homework2
     public class MaxFilter : SpatialFilter 
   {
     public MaxFilter(string name) : base(name) { }
-    protected override byte Operation(int a, int b, int x, int y, byte[][] input, Hashtable values)
+    protected override int Operation(int a, int b, int x, int y, int[][] input, Hashtable values)
     {
       int width = input.Length;
       int height = input[0].Length;
-      byte max = (byte)0;
+      byte maxR = (byte)0,
+           maxG = (byte)0,
+           maxB = (byte)0;
       for(int s = -a; s < a; s++)
       {
         int wX = x + s;
         if(wX < 0 || wX >= width)
           continue;
-				byte[] iX = input[wX];
+        byte[] iX = input[wX];
         for(int t = -b; t < b; t++)
         {
           int wY = y + t;
           if(wY < 0 || wY >= height)
             continue;
-          max = Math.Max(max, iX[wY]);
+          Color c = Color.FromArgb(iX[wY]);
+          if(c.R == c.B && c.B == c.G) 
+          {
+            int value = c.R;
+            maxR = Math.Max(maxR, value);
+            maxG = Math.Max(maxG, value);
+            maxB = Math.Max(maxB, value);
+          }
+          else
+          {
+            maxR = Math.Max(maxR, c.R);
+            maxG = Math.Max(maxG, c.G);
+            maxB = Math.Max(maxB, c.B);
+          }
         }
       }
-      return max;
+      return Color.FromArgb(255, maxR, maxG, maxB).ToArgb();
     }
   }
 }
